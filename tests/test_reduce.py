1239623e7e23d7c51e864f715c0908ef2c0d2765
caffeine-potent <ottoxwagner@gmail.com>
2018-07-05 15:41:58 -0400

    Refactor reduce to handle edge-case streams of length 0 and 1.

--- tests/test_reduce.py
+++ tests/test_reduce.py
@@ -11,6 +11,10 @@ class TestMisc(unittest.TestCase):
         _ = ms.stream([1,2,3,4,5]).reduce(sum_reduction)
         assert _ is 15
 
+    def test_initializer(self):
+        _ = ms.stream([1]).reduce(sum_reduction, initializer= 1)
+        assert _ is 2
+
     def test_reduce_with_one_element(self):
         _ = ms.stream([1]).reduce(sum_reduction)
         assert _ is 1
